4a023b5aec4ab8fd2c0fbac83351b8580b1bca12
Olena Syrotenko <olena.syrotenko@nure.ua>
2022-08-21 10:43:50 +0300

    Made data path as variable

--- populate-vault.ps1
+++ populate-vault.ps1
@@ -1,12 +1,8 @@
-param(
-    [string]$d
-)
-
 $env:VAULT_ADDR = "http://127.0.0.1:8200"
 $env:VAULT_TOKEN = "hvs.wa94spdg9rBYRJa5vHlDqYS5"
 $VENV_PATH = "D:\projects\own\vault-desc-ui\venv\venv\Scripts\python.exe"
 $SCRIPT_PATH = "json-data-to-vault-commands.py"
-$ARGUMENT_LIST = "-d" + $d
+$SCRIPT_ARGUMENT = "-d .\\vault-test-data.json"
 
 function GET_COMMAND {
     param(
@@ -24,7 +20,7 @@ function EXECUTE_COMMAND{
     Invoke-Expression $command
 }
 
-$commands = & $VENV_PATH $SCRIPT_PATH $ARGUMENT_LIST
+$commands = & $VENV_PATH $SCRIPT_PATH $SCRIPT_ARGUMENT
 foreach ($cmd in $commands) {
     $patch_command = GET_COMMAND -command_name 'patch' -data $cmd
     EXECUTE_COMMAND -command $patch_command
